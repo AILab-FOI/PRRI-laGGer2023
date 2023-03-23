17620b587e6d85940f068ccae62d1c041e2c6630
Teodor Kolar <tkolar20@foi.hr>
2023-03-23 01:36:35 +0100

    Updated sync script, fixed script line endings

--- zsync.sh
+++ zsync.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 #rsync -avu "/media/sf_laGGer-dev/" "/home/lagger/src" 
-rsync -avu --exclude "Python-3.6.9" --exclude "libwebsockets" \
+rsync -avu --delete --exclude "Python-3.6.9" --exclude "libwebsockets" \
  --exclude  "libnice" --exclude "janus-gateway" \
  --exclude "paho.mqtt.c" --exclude "rabbitmq-c"  \
- "/media/sf_laGGer-dev"  "/home/lagger/src/" 
+ --exclude "x11docker" --exclude "__pycache__" \
+ --exclude ".480x320" --exclude ".5000x5000" \
+ --exclude "addr.txt~" --exclude "install.sh~" \
+ --exclude "logfile_game.txt" --exclude "logfile_vnc.txt" \
+ --exclude "Python-3.6.9.tgz" --exclude "rebuild.sh~" \
+ --exclude "start_root_first.sh~" --exclude "start.sh~" \
+ --exclude "test.log" --exclude "test.py~"\
+ --exclude ".git" --exclude ".vscode"\
+ "/media/sf_laGGer-dev/"  "/home/lagger/src" 
+
