3b056e77b2fda63dcf336bd31e1567c1a8f58e5b
Fran Geček <fgecek20@foi.hr>
2023-05-22 22:26:47 +0200

    Finished virtual_sink.py which contains necessary tools for creation and deletion of virtual sinks. The script has been tested and works properly.

---
+++ virtual_sink.py
@@ -0,0 +1,21 @@
+import time
+import subprocess
+import re
+
+class VirtualSink:
+
+    sinkName = None
+    sinkIndex = None
+    greppingTheList = 'pacmd list-modules | grep -B 2 '
+
+    def __init__(self):
+        self.sinkName = 'VirtualSink' + str(int(time.time()))
+        self.greppingTheList += self.sinkName + " | head -n 1"
+        subprocess.run(["pacmd", "load-module", "module-null-sink", "sink_name=" + self.sinkName])
+        capturedOutput = subprocess.run([self.greppingTheList], shell=True, capture_output=True, text=True)
+        self.sinkIndex = capturedOutput.stdout
+        self.sinkIndex = re.sub('\D', "", self.sinkIndex)
+
+
+def DeleteSink(sinkIndex):
+        subprocess.run(["pactl", "unload-module", sinkIndex])
